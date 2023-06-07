69ed708b9d80a9eae3cd5e2a48256c69edba191d
sofisando <100186142+sofisando@users.noreply.github.com>
2023-06-07 19:37:10 -0300

    escribir en archivo

---
+++ escribir.py
@@ -0,0 +1,4 @@
+archivo= open ('archivo.txt','w') #la w es un modo, se llama write 
+archivo.write('HOLA MUNDO\n') #abrimos para escribir 
+archivo.write ('alo buenas')
+archivo.close() #y luego cerramos
